3a65ce4e6beb6668c637f54d339cc14e186bdda3
rimmesbe <ryanimmesberger@gmail.com>
2015-12-19 18:22:30 -0500

    created baby keepsake seeds

--- Cergamics/db/schema.rb
+++ Cergamics/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151214192038) do
+ActiveRecord::Schema.define(version: 20151219221836) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,19 @@ ActiveRecord::Schema.define(version: 20151214192038) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "baby_keepsakes", force: :cascade do |t|
+    t.string   "name",                                       null: false
+    t.string   "description"
+    t.string   "finish"
+    t.decimal  "price",              precision: 8, scale: 2
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+    t.datetime "created_at",                                 null: false
+    t.datetime "updated_at",                                 null: false
+  end
 
   create_table "finishes", force: :cascade do |t|
     t.text     "description"
