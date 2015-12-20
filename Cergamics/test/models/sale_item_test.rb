4bfe4a721d8a96e16b9f4c9ce49e1958d4e7c2ea
rimmesbe <ryanimmesberger@gmail.com>
2015-12-19 20:14:29 -0500

    created tests for sale_items

--- Cergamics/test/models/sale_item_test.rb
+++ Cergamics/test/models/sale_item_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class SaleItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @sale_item = sale_items(:saleitem)
+  end
+
+  should validate_presence_of(:name)
+
+  test "valid with all attributes" do
+    assert @sale_item.valid?, "Sale item is not valid"
+  end
 end
